54935766cc95a0609359ede746e10e4af729c493
Carlos Bartolome <carlos.bartolome@live.com>
2021-06-20 13:25:35 +0200

    Added some Button props

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,24 @@ import "./App.css";
 import Button from "./components/Button";
 
 function App() {
+  const handleButtonClick = () => {
+    alert("Someone has clicked me!");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <Button>I am a button</Button>
+        </div>
+        <div>
+          <Button disabled>I am a disabled button</Button>
+        </div>
+        <div>
+          <Button onClick={handleButtonClick} color="green">
+            I am a green clickable button
+          </Button>
         </div>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,27 @@ import "./Button.css";
 
 interface IButtonProps {
   children: React.ReactNode;
+  disabled?: boolean;
+  onClick?: () => void;
+  color?: "green" | "red";
 }
 
 const Button = (props: IButtonProps) => {
-  return <button className="Button">{props.children}</button>;
+  const { disabled, onClick, color } = props;
+
+  return (
+    <button
+      className="Button"
+      disabled={disabled}
+      onClick={onClick}
+      style={{
+        backgroundColor:
+          color === "green" ? "green" : color === "red" ? "red" : undefined,
+      }}
+    >
+      {props.children}
+    </button>
+  );
 };
 
 export default Button;
